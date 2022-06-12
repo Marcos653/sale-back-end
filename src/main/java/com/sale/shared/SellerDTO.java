e00ea285164b66139061e58a35760091f489a3e9
Your Name <viniciustatuta@gmail.com>
2022-06-11 22:45:16 -0300

    feat: sellerDTO

--- src/main/java/com/sale/shared/SellerDTO.java
+++ src/main/java/com/sale/shared/SellerDTO.java
@@ -1,5 +1,44 @@
 package com.sale.shared;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.sale.Model.Sale;
+
 public class SellerDTO {
+
+    private Long id;
+    private String name;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime created_at = LocalDateTime.now();
+    private List<Sale> sales = new ArrayList<>();
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Sale> getSales() {
+        return sales;
+    }
+
+    public void setSales(List<Sale> sales) {
+        this.sales = sales;
+    }
+  
     
 }
