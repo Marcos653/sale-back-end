7c39f17e6267bafac1200ff8dcee4e00d1ec0de4
Your Name <viniciustatuta@gmail.com>
2022-06-11 23:04:01 -0300

    feat: handlers

--- src/main/java/com/sale/view/handlers/ErrorFormDTO.java
+++ src/main/java/com/sale/view/handlers/ErrorFormDTO.java
@@ -1,5 +1,31 @@
 package com.sale.view.handlers;
 
 public class ErrorFormDTO {
+
+    private String field;
+    private String error;
+
+    public ErrorFormDTO(String field, String error) {
+        this.field = field;
+        this.error = error;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+
     
 }
