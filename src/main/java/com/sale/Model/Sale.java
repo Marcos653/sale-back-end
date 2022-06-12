8c32e791b260455ccd912d1866734193c21796f4
Your Name <viniciustatuta@gmail.com>
2022-06-11 22:25:36 -0300

    feat: sale model

--- src/main/java/com/sale/Model/Sale.java
+++ src/main/java/com/sale/Model/Sale.java
@@ -1,5 +1,80 @@
 package com.sale.Model;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+@Table(name = "sales")
 public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "created_at")
+    private LocalDate created_at = LocalDate.now();
+    @Column(name = "price")
+    private Double price;
+
+    @ManyToOne
+    @JsonBackReference
+    @JoinColumn(name="seller_id", nullable=false)
+    private Seller seller;
+
+
+    public Sale() {
+    }
+
+
+    public Sale(Long id, LocalDate created_at, Double price, Seller seller) {
+        this.id = id;
+        this.created_at = created_at;
+        this.price = price;
+        this.seller = seller;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDate created_at) {
+        this.created_at = created_at;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Seller getSeller() {
+        return seller;
+    }
+
+    public void setSeller(Seller seller) {
+        this.seller = seller;
+    }
+
+    
     
 }
