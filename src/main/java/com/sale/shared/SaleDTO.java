ebff0746456093858b51bdfcfa682a01322acdaa
Your Name <viniciustatuta@gmail.com>
2022-06-11 22:42:27 -0300

    feat: saleDTO

--- src/main/java/com/sale/shared/SaleDTO.java
+++ src/main/java/com/sale/shared/SaleDTO.java
@@ -1,5 +1,52 @@
 package com.sale.shared;
 
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.sale.Model.Seller;
+
 public class SaleDTO {
     
+    private Long id;
+    private Integer sale_amount;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime created_at = LocalDateTime.now();
+
+    private Seller seller;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getSale_amount() {
+        return sale_amount;
+    }
+
+    public void setSale_amount(Integer sale_amount) {
+        this.sale_amount = sale_amount;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    public Seller getSeller() {
+        return seller;
+    }
+
+    public void setSeller(Seller seller) {
+        this.seller = seller;
+    }
+
+
+    
+
 }
