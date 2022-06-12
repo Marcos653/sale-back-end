018626485b94e4af37bb71ae918db1d8413d9726
Your Name <viniciustatuta@gmail.com>
2022-06-11 23:50:43 -0300

    refactor: sellerResponse

--- src/main/java/com/sale/view/model/SellerResponse.java
+++ src/main/java/com/sale/view/model/SellerResponse.java
@@ -1,17 +1,29 @@
 package com.sale.view.model;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.sale.Model.Sale;
 
 public class SellerResponse {
     
     private Long id;
     private String name;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime created_at = LocalDateTime.now();
     private List<Sale> sale = new ArrayList<>();
 
     
+
+    
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
     public Long getId() {
         return id;
     }
