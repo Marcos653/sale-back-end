f20c11da15f274a868c1c5fbc259a4024e0f2618
Your Name <viniciustatuta@gmail.com>
2022-06-11 22:32:14 -0300

    feat: seller repository

--- src/main/java/com/sale/repository/SellerRepository.java
+++ src/main/java/com/sale/repository/SellerRepository.java
@@ -1,5 +1,11 @@
 package com.sale.repository;
 
-public interface SellerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sale.Model.Seller;
+
+@Repository
+public interface SellerRepository extends JpaRepository<Seller, Long> {
     
 }
