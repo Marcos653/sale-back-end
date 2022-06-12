a5ba6ee3c83317b88bc940565ab74204e446a142
Your Name <viniciustatuta@gmail.com>
2022-06-11 23:00:50 -0300

    feat: seller service

--- src/main/java/com/sale/service/SellerService.java
+++ src/main/java/com/sale/service/SellerService.java
@@ -1,5 +1,16 @@
 package com.sale.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.sale.Model.Sale;
+import com.sale.shared.SellerDTO;
+
 public interface SellerService {
-    
+    List<SellerDTO> getAllSeller();
+    SellerDTO register(SellerDTO seller);
+    Optional<SellerDTO> getById(Long id);
+    void deleteSeller(Long id);
+    SellerDTO updateSeller(Long id, SellerDTO newSeller);
+    SellerDTO partialUpdateSeller(Long id, String name, List<Sale> sale);
 }
--- src/main/java/com/sale/service/SellerServiceImpl.java
+++ src/main/java/com/sale/service/SellerServiceImpl.java
@@ -1,5 +1,91 @@
 package com.sale.service;
 
-public class SellerServiceImpl {
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sale.Model.Sale;
+import com.sale.Model.Seller;
+import com.sale.repository.SellerRepository;
+import com.sale.shared.SellerDTO;
+
+@Service
+public class SellerServiceImpl implements SellerService {
+
+    @Autowired
+    private SellerRepository repo;
+
+    @Override
+    public List<SellerDTO> getAllSeller() {
+        List<Seller> sellers = repo.findAll();
+        return sellers.stream()
+            .map(s -> new ModelMapper().map(s, SellerDTO.class))
+            .collect(Collectors.toList());
+    }
+
+    @Override
+    public SellerDTO register(SellerDTO seller) {
+        return salveSeller(seller);
+    }
+
+    @Override
+    public Optional<SellerDTO> getById(Long id) {
+        Optional<Seller> seller = repo.findById(id);
+
+        if(seller.isPresent()){
+             return Optional.of(new ModelMapper()
+                 .map(seller.get(), SellerDTO.class)); 
+        }
+ 
+         return Optional.empty();
+    }
+
+    @Override
+    public void deleteSeller(Long id) {
+        repo.deleteById(id);
+        
+    }
+
+    @Override
+    public SellerDTO updateSeller(Long id, SellerDTO newSeller) {
+        newSeller.setId(id);
+        return salveSeller(newSeller);
+    }
+
+    private SellerDTO salveSeller(SellerDTO seller) {
+        ModelMapper mapper = new ModelMapper();
+        Seller seller_entity = mapper.map(seller, Seller.class);
+        seller_entity = repo.save(seller_entity);
+        return mapper.map(seller_entity, SellerDTO.class);
+    }
+
+    @Override
+    public SellerDTO partialUpdateSeller(Long id, String name, List<Sale> sale) {
+        ModelMapper mapper = new ModelMapper();
+        Optional<Seller> seller = repo.findById(id);
+
+        if(seller.isPresent()){
+
+            if(name != null){
+                seller.get().setName(name);
+                repo.save(seller.get());
+            }
+
+            if(sale != null){
+                seller.get().setSale(sale);
+                repo.save(seller.get());
+            }
+
+            return mapper.map(seller.get(), SellerDTO.class);
+
+        }
+
+        return null;
+        
+    }
     
 }
