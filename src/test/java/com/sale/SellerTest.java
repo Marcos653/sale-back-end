f1ff651d882216e37b74f45f00ac002eda662c9a
Your Name <viniciustatuta@gmail.com>
2022-06-13 12:09:58 -0300

    feat: seller test unit

--- src/test/java/com/sale/SellerTest.java
+++ src/test/java/com/sale/SellerTest.java
@@ -1,5 +1,50 @@
 package com.sale;
 
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.sale.Model.Seller;
+import com.sale.repository.SellerRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SellerTest {
+
+    @Autowired
+    private SellerRepository sellerRepository;
+  
+  
+    @Test
+    public void registerSeller() {
+      Seller seller = new Seller(1L, "Baker uncle", null);
+      Seller saveSeller = sellerRepository.save(seller);
+      Assert.assertEquals(saveSeller.getName(), seller.getName());
+    }
+  
+    @Test()
+    public void registerWithoutName() {
+      Seller seller = new Seller(1L, null, null);
+      sellerRepository.save(seller);
+    }
+  
+    @Test
+    public void getAllSeller() {
+      Seller firstSeller= new Seller(1L, "Banker", null);
+      Seller secondSeller = new Seller(2L, "Butcher", null);
+      Seller thirdSeller = new Seller(3L, "Lawyer", null);
+  
+      sellerRepository.save(firstSeller);
+      sellerRepository.save(secondSeller);
+      sellerRepository.save(thirdSeller);
+      List<Seller> sellers = sellerRepository.findAll();
+  
+      Assert.assertEquals(sellers.size(), 12);
+    }
 
 }
